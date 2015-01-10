e7d7a61ef6c47aee1c1d245c9f8c01cbf73bf5e6
otavioNicolau <otavio.nicollau@gmail.com>
2015-01-10 21:47:52 -0200

    1.0.15
    scripts creaturescripts

--- data/creaturescripts/scripts/conected.lua
+++ data/creaturescripts/scripts/conected.lua
@@ -1,9 +1,6 @@
 function onLogin(cid, words, param, channel)
 local health = getCreatureMaxHealth(cid)
-local ActualPos = getCreaturePosition(cid) 
 local mana = getCreatureMaxMana(cid)
-local tmp = {accountId = getPlayerAccountId(pid), ip = getPlayerIp(pid)}
-local pos = getCreaturePosition(pid)
 local quests = {9393, 12568, 12618, 5808, 7424 ,2000 ,12617 ,8931 ,8932 ,8925 ,2495 ,2646 ,6132 ,2527 ,6391 ,6528}
 local feitas = {}
 playerpos = getPlayerPosition(cid)
@@ -13,11 +10,11 @@ doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, ""..getPlayerName(ci
 doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_RED, "voce tem ".. getPlayerPremiumDays(cid) .." dias de premium account.")
 doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_ORANGE, "Voce tem "..health.." health points.")
 doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_BLUE, "Voce tem "..mana.." mana points.")
-doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_RED, "&#8226;Suas Informacoes:" ..
-			    "\n&#164;Name: " .. getCreatureName(cid) ..
-			    "\n&#164;Level: " .. getPlayerLevel(cid) ..
-			    "\n&#164;Magic Level: "..getPlayerMagLevel(cid)..
-			    "\n&#164;Vocation: " .. getVocationInfo(getPlayerVocation(cid)).name ..")")
+doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_RED, "Suas Informacoes:" ..
+			    "\nName: " .. getCreatureName(cid) ..
+			    "\nLevel: " .. getPlayerLevel(cid) ..
+			    "\nMagic Level: "..getPlayerMagLevel(cid)..
+			    "\nVocation: " .. getVocationInfo(getPlayerVocation(cid)).name ..")")
 	    doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have " .. getPlayerMoney(cid) .. " gold in your backpack.")
 doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "Your account balance is " .. getPlayerBalance(cid) .. ".")
 doCreatureAddHealth(cid,1000000)
@@ -26,7 +23,7 @@ doPlayerSendTextMessage(cid, 27, getPlayerName(cid) .. " completou " .. #feitas 
 doBroadcastMessage(getPlayerName(cid) .. " [" .. getPlayerLevel(cid) .. "]: ENTREI!", MESSAGE_INFO_DESCR)
 file = io.open('noticias.txt','r')
 notice = file:read(-1)
-doShowTextDialog(cid,7528,notice)
+doPlayerPopupFYI(cid,notice)
 file:close()
 return TRUE
 end
