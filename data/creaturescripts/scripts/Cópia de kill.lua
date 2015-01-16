f7c25a2bc49e3bad2f0fc42ca5a8746acb6b6bb5
otavioNicolau <otavio.nicollau@gmail.com>
2015-01-16 21:03:03 -0200

    1.1.2
    Exclusão de scripts que estão sem uso

--- data/creaturescripts/scripts/Cópia de kill.lua
+++
@@ -1,9 +0,0 @@
-function onKill(cid, target)
-	local monster = getCreatureName(target)
-	local room = getArenaMonsterIdByName(monster)
-	if room > 0 then
-		setPlayerStorageValue(cid, room, 1)
-		doPlayerSendTextMessage(cid,MESSAGE_EVENT_DEFAULT,'Voce pode ir para a proxima sala!')
-	end
-	return TRUE
-end
