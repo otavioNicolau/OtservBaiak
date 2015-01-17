edcfab8a3e15dc127daf510da8921aa8fba46743
otavioNicolau <otavio.nicollau@gmail.com>
2015-01-16 22:14:50 -0200

    1.1.4
    Exclusão de scripts sem uso.

--- data/actions/scripts/orangetree.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-if item.itemid == 4006 then
-doTransformItem(item.uid,4008)
-doPlayerAddItem(cid,2675,3)
-doDecayItem(item.uid)
-end
-return 1 
-end
--- data/actions/scripts/window_close.lua
+++
@@ -1,7 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-	if getTileHouseInfo(getPlayerPosition(cid)) ~= 0 then
-		return doTransformItem(item.uid, item.itemid - 2)
-	else
-		return 0	
-	end
-end
