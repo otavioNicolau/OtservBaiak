c7ff696afba7b219fca254d6251d4d7c7ca3e9eb
otavioNicolau <otavio.nicollau@gmail.com>
2015-01-18 07:44:41 -0200

    Baiak Paranormal
    Organização do codigo de informações!

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1>Baiak Paranormal</h1>
-<p>Versão: 1.1.16</p>
--- data/talkactions/scripts/acritical.lua
+++ data/talkactions/scripts/acritical.lua
@@ -1,7 +1,7 @@
 --- Criado por Absolute ---
 function onSay(cid, words, param)
-file = io.open('critical.txt','r')
+file = io.open('info/critical.txt','r')
 notice = file:read(-1)
-doShowTextDialog(cid,7528,notice)
+doPlayerPopupFYI(cid,notice)
 file:close()
 end
